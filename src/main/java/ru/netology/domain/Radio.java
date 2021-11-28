101e597f71d8e613d56f6cf86b0a42bceb95aa07
AlexandrP88 <alexsandr@rtural.ru>
2021-11-29 01:09:23 +0500

    modified Radio & RadioTest v2

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,13 +3,34 @@ package ru.netology.domain;
 public class Radio {
     private int currentStation;
     private int currentVolume;
+    private int maxStation = 10;
+    private int firstStation;
+
+    public Radio() {
+    }
+
+    public Radio(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        if (maxStation <= 0) {
+            return;
+        }
+        this.maxStation = maxStation;
+
+    }
 
     public int getCurrentStation() {
         return currentStation;
     }
 
     public void setCurrentStation(int currentStation) {
-        if (currentStation > 9) {
+        if (currentStation > maxStation - 1) {
             return;
         }
         if (currentStation < 0) {
@@ -25,7 +46,7 @@ public class Radio {
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume > 10) {
+        if (currentVolume > 100) {
             return;
         }
         if (currentVolume < 0) {
@@ -35,36 +56,35 @@ public class Radio {
     }
 
     public void increaseStation() {
-        if (currentStation == 9) {
+
+        if (currentStation < maxStation) {
             currentStation = currentStation + 1;
         }
 
-        if (currentStation < 9) {
-            currentStation = currentStation + 1;
-        }
-
-        if (currentStation == 10) {
-            currentStation = 0;
+        if (currentStation >= maxStation) {
+            currentStation = firstStation;
         }
     }
 
 
     public void reduceStation() {
 
-        if (currentStation == 0) {
+        if (currentStation == firstStation) {
             currentStation = currentStation - 1;
         }
-        if (currentStation >= 0) {
+
+        if (currentStation > firstStation) {
             currentStation = currentStation - 1;
         }
-        if (currentStation == -1) {
-            currentStation = 9;
+
+        if (currentStation <= firstStation - 1) {
+            currentStation = maxStation - 1;
         }
 
     }
 
     public void increaseVolume() {
-        if (currentVolume < 10) {
+        if (currentVolume < 100) {
             currentVolume = currentVolume + 1;
         }
 
